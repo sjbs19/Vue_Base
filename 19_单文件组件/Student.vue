72d3ab47756fa35a56d684123fd70ca7c60f8974
sjbs19 <2217839344@qq.com>
2022-08-13 17:15:24 +0800

    Vue基础

---
+++ 19_单文件组件/Student.vue
@@ -0,0 +1,29 @@
+<template>
+  <!-- 组件的结构 -->
+  <div class="demo">
+    <h2>学生姓名:{{name}}</h2>
+    <h2>学生年龄:{{age}}</h2>
+  </div>
+</template>
+
+<script>
+    // 组件交互相关的代码（数据，方法等）
+    //创建student组件
+    //默认暴露
+    export default {
+        name:'Student',
+        data(){
+            return {
+              name:'zs',
+              age:18,
+            }
+        }
+    }
+</script>
+
+<style>
+  /*组件的样式*/
+  .demo{
+    background-color: red;
+  }
+</style>
---
+++ 19_单文件组件/main.js
@@ -0,0 +1,10 @@
+//引入组件
+import App from './App.vue'
+//创建vm实例
+new Vue({
+  el:'#root',
+  template:`<App></App>`,
+  components:{
+    App
+  }
+})
